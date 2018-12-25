417fd376fbe8391476a651dca1a9670dc3b0e53c
chengyang123456789 <39935866+chengyang123456789@users.noreply.github.com>
2018-12-25 17:40:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,77 @@
 var http = require('http')
-var url = require('url')
-var fs = require('fs')
 var path = require('path')
+var fs = require('fs')
+var url = require('url')
 
-var server = http.createServer(function(req,res){
-    var pathObj = url.parse(req.url,true)
-    var direct = path.join(__dirname,'sample')
-    var filePath = path.resolve(direct,pathObj.pathname)
-    
-    fs.readFile(filePath,'binary',function(err,fileContent){
-        if(err){
-            console.log('404')
-            res.writeHead(404,'not found')
-            res.end('<h1>404 Not  Found</h1>')
-        }else{
-            console.log('ok')
-            res.writeHead(200,'ok')
-            res.write(fileContent,'binary')
-            res.end()
-        }
-    })
+var routes = {
+    '/a': function(req, res){
+        res.end('match /a, query is:' + JSON.stringify(req.query))
+    },
+
+    '/b': function(req, res){
+        res.end('match /b')
+    },
+
+    '/a/c.js': function(req, res){
+        res.end('match /a/c.js')
+    },
+
+    '/search': function(req, res){
+        res.end('username='+req.body.username+',password='+req.body.password)
+    }
+
+}
+
+
+var server = http.createServer(function(req, res){
+    routePath(req, res)
 })
-server.listen(9000)
+
+server.listen(8080)
+
+
+
+function routePath(req, res){
+    var pathObj = url.parse(req.url, true)
+    var handleFn = routes[pathObj.pathname]
+
+    if(handleFn){
+        req.query = pathObj.query
+
+        var body = ''
+        req.on('data', function(chunk){
+            body += chunk
+        }).on('end', function(){
+            req.body = parseBody(body)
+            handleFn(req, res)
+        })
+
+    }else {
+        staticRoot(path.resolve(__dirname, 'sample'), req, res)
+    }
+}
+
+function staticRoot(staticPath, req, res){
+    var pathObj = url.parse(req.url, true)
+    var filePath = path.resolve(staticPath, pathObj.pathname)
+
+    fs.readFile(filePath,'binary', function(err, content){
+        if(err){
+            res.writeHead('404', 'haha Not Found')
+            return res.end()
+        }
+
+        res.writeHead(200, 'Ok')
+        res.write(content, 'binary')
+        res.end()
+    })
+
+}
+
+function parseBody(body){
+    var obj = {}
+    body.split('&').forEach(function(str){
+        obj[str.split('=')[0]] = str.split('=')[1]
+    })
+    return obj
+}
